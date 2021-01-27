3e917014ebcda3aea7124ca3dc09c6fc19bb750f
j9972 <j9972@naver.com>
2021-01-28 00:29:14 +0900

    mysql 시작

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "crud_pedro",
+})
 
 app.get('/' , (req,res) => {
-	res.send('hello, this port number is 3001');
+
+    const sqlInsert = 
+      "INSERT INTO movie_review (movieName, movieReview) VALUES ('inception', 'good movie')"
+    db.query(sqlInsert, (err,result) => {
+        res.send("hello pedro");
+    })
+	//res.send('hello, this port number is 3001 devStart'); -> mysql 쓰고 나서 이 코드가 남아 있으면 에러를 일으킨다.
 })
 
 app.listen(3001, () => {
