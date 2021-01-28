641ae03a5052995fa871348ea0bf51ed666ca42e
j9972 <j9972@naver.com>
2021-01-28 12:57:56 +0900

    body-parser 시작 부분까지 실행함

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
 
   const [movieName, setMovieName] = useState("")
   const [review, setReview] = useState("")
+
+  const submitReview = () => {
+    Axios.post('http://localhost:3001/api/insert' , {
+      movieName: movieName, 
+      movieReview : review
+    }).then(() => {
+      alert("successful insert")
+    });
+  }
 
   return (
     <div className="App">
@@ -21,7 +31,7 @@ function App() {
           setReview(e.target.value)
         }} />
 
-        <button>Submit</button>
+        <button onClick={submitReview}>Submit</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const mysql = require('mysql')
 
@@ -7,8 +8,18 @@ const db = mysql.createPool({
     user: "root",
     password: "password",
     database: "crud_pedro",
-})
+});
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/api/insert', (req,res) => {
+    const sqlInsert = "INSERT INTO `movie_review` (`movieName`, `movieReview`) VALUES (?,?)"
+    db.query(sqlInsert, [movieName, movieReview], (err,result) => {
+
+    })
+});
+
+/*
 app.get('/' , (req,res) => {
 
     // 2. db라는 const가 실행이 되는지 확인을 위해 달아놓은 코드
@@ -21,6 +32,7 @@ app.get('/' , (req,res) => {
 
 	//1.파일 자체가 실행되는지 확인 차 만들었던 코드  res.send('hello, this port number is 3001 devStart'); -> mysql 쓰고 나서 이 코드가 남아 있으면 에러를 일으킨다.
 })
+*/
 
 app.listen(3001, () => {
 	console.log('running on port 3001');
