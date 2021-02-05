0a50eec55f8124a681be64ef3279d96859942129
j9972 <j9972@naver.com>
2021-02-05 22:33:25 +0900

    add code

--- server/index.js
+++ server/index.js
@@ -49,6 +49,17 @@ app.get('/' , (req,res) => {
 })
 */
 
+app.post('/api/delete', (req,res) => {
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO `movie_review` (`movieName`, `movieReview`) VALUES (?,?)"
+    db.query(sqlInsert, [movieName, movieReview], (err,result) => {
+        console.log(result);
+    });
+});
+
 app.listen(3001, () => {
 	console.log('running on port 3001');
 });  
